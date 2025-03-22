1233d18e26fd3f78016bfe7c2ddb307841949672
Poolcero <poolcerodragon@gmail.com>
2025-03-22 14:07:46 -0400

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,54 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 100000000; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+  
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+    
+    // Controlar la flor
+    var flor = document.querySelector(".flor");
+    if (flor) {
+      flor.style.animation = "fadeIn 60s ease-in-out forwards"; // Añadir animación de flor
+      flor.style.opacity = opacity; // Control de la opacidad de la flor
+    }
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+    
+    // Ocultar la flor lentamente
+    var flor = document.querySelector(".flor");
+    if (flor) {
+      flor.style.animation = "none"; // Detiene la animación
+      flor.style.opacity = 0; // Hace que la flor se desvanezca
+    }
+  }
+  
+  }
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
